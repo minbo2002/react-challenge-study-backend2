2d73134785981d6b52d0ae71dc6a07c44fa4d1ad
minbo2002 <minbo2002@naver.com>
2023-11-09 04:18:24 +0900

    feat: 프로젝트 첫 환경설정

---
+++ src/main/java/com/example/reactchallengestudybackend2/ReactChallengeStudyBackend2Application.java
@@ -0,0 +1,13 @@
+package com.example.reactchallengestudybackend2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactChallengeStudyBackend2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactChallengeStudyBackend2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/reactchallengestudybackend2/ReactChallengeStudyBackend2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactchallengestudybackend2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactChallengeStudyBackend2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
